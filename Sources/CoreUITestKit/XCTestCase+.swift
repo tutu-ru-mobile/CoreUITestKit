afa2ed9454af1e3634b9a143679f00bbd52b1033
VeriginSergey <102799757+VeriginSergey@users.noreply.github.com>
2023-09-02 00:45:10 +0300

    исправил грамматическую ошибку

--- Sources/CoreUITestKit/XCTestCase+.swift
+++ Sources/CoreUITestKit/XCTestCase+.swift
@@ -3,34 +3,34 @@ import Foundation
 import XCTest
 
 extension XCTestCase {
-    public func dsl_XCTAssertTrue(XCUIElement: XCUIElement, action: XCUIElement.Action, errorMassage: @autoclosure () -> String, file: StaticString = #file, line: UInt = #line) {
+    public func dsl_XCTAssertTrue(XCUIElement: XCUIElement, action: XCUIElement.Action, errorMessage: @autoclosure () -> String, file: StaticString = #file, line: UInt = #line) {
         XCTContext.runActivity(named: "Проверка элемента '\(XCUIElement)' на \(action.self)" ) { _ in
             XCTAssertTrue(
                 XCUIElement.checkAction(action),
-                errorMassage(),
+                errorMessage(),
                 file: file,
                 line: line
             )
         }
     }
 
-    public func dsl_XCTAssertFalse(XCUIElement: XCUIElement, action: XCUIElement.Action, errorMassage: @autoclosure () -> String, file: StaticString = #file, line: UInt = #line) {
+    public func dsl_XCTAssertFalse(XCUIElement: XCUIElement, action: XCUIElement.Action, errorMessage: @autoclosure () -> String, file: StaticString = #file, line: UInt = #line) {
         XCTContext.runActivity(named: "Проверка элемента '\(XCUIElement)' на не \(action.self)" ) { _ in
             XCTAssertFalse(
                 XCUIElement.checkAction(action),
-                errorMassage(),
+                errorMessage(),
                 file: file,
                 line: line
             )
         }
     }
 
-    public func dsl_XCTAssertEqual<T: Equatable>(expression1: T, expression2: T, errorMassage: @autoclosure () -> String, file: StaticString = #file, line: UInt = #line) {
+    public func dsl_XCTAssertEqual<T: Equatable>(expression1: T, expression2: T, errorMessage: @autoclosure () -> String, file: StaticString = #file, line: UInt = #line) {
         XCTContext.runActivity(named: "Проверить значение \(expression1) на равенство с \(expression2)") { _ in
             XCTAssertEqual(
                 expression1,
                 expression2,
-                errorMassage(),
+                errorMessage(),
                 file: file,
                 line: line
             )
